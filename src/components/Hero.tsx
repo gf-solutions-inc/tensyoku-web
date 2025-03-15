dd50e2013afea0323becf4f18eeeef2a853c4171
dokimiki <57443812+dokimiki@users.noreply.github.com>
2025-03-15 17:10:33 +0900

    imgのパスを変更

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,7 @@ export default function Hero() {
     return (
         <div className="w-full">
             <div className="base-image">
-                <Image src="/img/assets/header.png" alt="メインの画像" width={856} height={476} priority />
+                <Image src="./img/assets/header.png" alt="メインの画像" width={856} height={476} priority />
             </div>
             {/* <p className="caution text-[10px] text-[#666] leading-[1.2] text-center pt-[10px]">※当サイトは一部広告収益を得て運営しております。</p> */}
         </div>
